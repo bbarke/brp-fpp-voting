5ca39baf165fb97737436e1a6f518acc18543dea
Brent Barker <brent@barkersrandomprojects.com>
2020-05-03 09:01:56 -0600

    finding bugs

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -x
 
-# fpp-plugin-Template install script
-
-sudo apt-get update && sudo apt-get install -y python-pip;
-python2.7 -m pip install requests;
+which python;
+python --version;
+python3 --version;
+sudo apt-get update && sudo apt-get install -y python3 python3-pip;
+python3 -m pip install requests;
+which python;
+python --version;
