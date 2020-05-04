46312ef80075c401f58d8a114f22380eb15b0a2b
Brent Barker <brent@barkersrandomprojects.com>
2020-05-03 22:38:10 -0600

    test update

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -x
 
-which python;
-python --version;
-python3 --version;
 sudo apt-get update && sudo apt-get install -y python3 python3-pip;
 python3 -m pip install requests;
-which python;
-python --version;
