39d15b725575a30d799bd7e8e0c2eb90fc48708c
Brent Barker <brentbarker9@gmail.com>
2025-09-22 21:45:20 -0600

    Revert "Add fpp 9 fix install again (#20)"
    This reverts commit f17679dc923df8fe377cb72b7fc110b193d80337.

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
 
-sudo apt-get update && sudo apt-get install -y python3-requests python3-crontab;
+sudo apt-get update && sudo apt-get install -y python3-pip;
+python3 -m pip install requests python-crontab;
